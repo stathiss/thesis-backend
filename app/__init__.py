5980251ebf3a7eb6547f7b10f19ef4345a007105
stathiss <geor_stath@hotmail.com>
2019-02-18 10:39:09 +0200

    Parse full tweet from tweet api

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import re
 import config
 import tweepy
 import json
@@ -10,15 +11,15 @@ app = Flask(__name__)
 def tweet_to_full_text(tweet):
     if 'retweeted_status' in tweet:
         if 'full_text' in tweet['retweeted_status']:
-            return tweet['retweeted_status']['full_text']
+            return re.sub(r"http\S+", "", tweet['retweeted_status']['full_text'])
         else:
-            return tweet['retweeted_status']['text']
+            return re.sub(r"http\S+", "", tweet['retweeted_status']['text'])
 
     else:
         if 'full_text' in tweet:
-            return tweet['full_text']
+            return re.sub(r"http\S+", "", tweet['full_text'])
         else:
-            return tweet['text']
+            return re.sub(r"http\S+", "", tweet['text'])
 
 
 @app.route('/', methods=["GET"])
@@ -35,17 +36,21 @@ def hello():
 
     api = tweepy.API(auth)
 
-    public_tweets = tweepy.Cursor(api.search, q='#' + hashtag, lang='en', count=100, tweet_mode='extended').items(100)
+    public_tweets = tweepy.Cursor(api.search, q='#' + hashtag, lang='en', count=200, tweet_mode='extended').items(200)
     tweets = []
+    ids = []
+    texts = []
     for tweet in public_tweets:
         # json_tweet = tweet._json
         full_tweet = tweet_to_full_text(tweet._json)
         print(json.dumps(tweet._json, indent=2))
-        tweets.append({
-            'text': full_tweet,
-            'id': tweet.id})
+        if not any(d['text'] == full_tweet for d in tweets):
+            tweets.append({
+                'text': full_tweet,
+                'id': tweet.id})
         print(tweet.full_text)
         print('\n')
+
     my_json = jsonify(tweets)
     my_json.headers.add('Access-Control-Allow-Origin', '*')
     return my_json
