17ff3fe2e7dcfabad0b0fab94e7c0708db5a4551
stathiss <geor_stath@hotmail.com>
2019-03-13 02:01:24 +0200

    Change the json of api call

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ import tweepy
 import json
 from flask import Flask, jsonify, request
 from tweepy import OAuthHandler
-from sources.utils import write_predictions
+from sources.utils import write_predictions, calculate_all_predictions
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -41,12 +41,12 @@ def hello():
 
     api = tweepy.API(auth)
 
-    public_tweets = tweepy.Cursor(api.search, q='#' + hashtag, lang='en', count=200, tweet_mode='extended').items(200)
+    public_tweets = tweepy.Cursor(api.search, q='#' + hashtag + ' ', lang='en', count=200, tweet_mode='extended').items(200)
     tweets = []
     ids = []
     texts = []
+    counter = 1
     for tweet in public_tweets:
-        # json_tweet = tweet._json
         full_tweet = tweet_to_full_text(tweet._json)
         print(json.dumps(tweet._json, indent=2))
         if not any(d['text'] == full_tweet for d in tweets):
@@ -54,10 +54,37 @@ def hello():
             texts.append(full_tweet.encode('utf-8').strip())
             tweets.append({
                 'text': full_tweet,
-                'id': tweet.id})
+                'id': tweet.id,
+                'counter': counter,
+                'author': tweet.user.name,
+                'date': tweet.created_at})
+            counter = counter + 1
+            print(tweet.created_at)
         print('\n')
-    write_predictions('test_tweets.txt', [ids, texts, ['emotion' for _ in range(len(ids))]], [0 for _ in range(len(ids))])
-    my_json = jsonify(tweets)
+    write_predictions('test_tweets.txt',
+                      [ids, texts, ['emotion' for _ in range(len(ids))]], [0 for _ in range(len(ids))])
+    predictions = calculate_all_predictions(tweets)
+    my_json = jsonify({
+        'tweets': tweets,
+        'top': {
+            'joy': {
+                'tweet': 'Hello Darkness my old friend',
+                'intensity': 0.76
+            },
+            'fear': {
+                'tweet': 'Hello Darkness my old friend',
+                'intensity': 0.76
+            },
+            'sadness': {
+                'tweet': 'Hello Darkness my old friend',
+                'intensity': 0.76
+            },
+            'anger': {
+                'tweet': 'Hello Darkness my old friend',
+                'intensity': 0.76
+            },
+        }
+    })
     my_json.headers.add('Access-Control-Allow-Origin', '*')
     return my_json
 # Import the routes.
